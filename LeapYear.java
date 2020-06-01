a611bbe4ef30a9ae72e4ddf66e7ca846270ea629
chuemoua3 <chuemoua12@outlook.com>
2020-06-01 16:31:34 -0400

    lab done

--- LeapYear.java
+++ LeapYear.java
@@ -2,6 +2,18 @@ package TDDLab.LeapYear;
 
 public class LeapYear {
     
-    
+    //method that takes in an int and checks for leap year
+
+    public static boolean isItLeapYear(int year){
+        if(year % 4 == 0){
+            if(year % 100 == 0 && year % 400 != 0){
+                return false;
+            }else {
+                return true;
+            }
+        }else {
+            return false;
+        }
+    }
 
 }
--- LeapYearTest.java
+++ LeapYearTest.java
@@ -1,17 +1,24 @@
 package TDDLab.LeapYear;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class LeapYearTest {
     
     @Test
     public void whenYearIsDivisibleBy4(){
-
+        assertEquals(true, LeapYear.isItLeapYear(1992));
+        assertEquals(false, LeapYear.isItLeapYear(2010));
+        assertEquals(false, LeapYear.isItLeapYear(1995));
     }
 
     @Test
     public void whenYearCannotBeDivisibleBy100ButIsDivisibleBy400(){
-        
+        assertEquals(false, LeapYear.isItLeapYear(1900));
+        assertEquals(true, LeapYear.isItLeapYear(2000));
+        assertEquals(true, LeapYear.isItLeapYear(1600));
+
     }
     
 
